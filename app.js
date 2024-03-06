88ee1f85287cffbde8b49f9af6932398babd95dd
oliveeira2610 <bielolirodrigues@gmail.com>
2024-03-06 17:28:11 -0300

    agora vai krlh

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const cors = require('cors');
-const config = require('./config.json');
+
+const express = require ('express');
+const axios = require ('axios');
+const path = require ('path');
+const cors = require ('cors');
+const config = require ('./config.json');
 const apikey = config.apikey;
+
 
 const app = express();
 
@@ -12,34 +14,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// const TraducaoClima = {
-//   "few clouds": "Poucas nuvens",
-//   "scattered clouds": "Nuvens dispersas",
-//   "overcast clouds": "Nubladas",
-//   "broken clouds": "Nuvens quebradas"
-// };
+const TraducaoClima = {
+    "few clouds": "Poucas nuvens",
+    "scattered clouds": "Nuvens dispersas",
+    "overcast clouds": "Nubladas",
+    "broken clouds": "Nuvens quebradas"
+}
 
+app.get('/climatempo/:cidade', async(req, res) => {
+    const city = req.params.cidade;
 
-app.get('/climatempo/:cidade', async (req, res) => {
-  const city = req.params.cidade;
+    try{
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
+        if (response.status === 200) {
+            const clima = TraducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
 
-  try {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`);
-    if (response.status === 200) {
-      const clima = TraducaoClima[response.data.weather[0].description] || response.data.weather[0].description;
-
-      const weatherData = {
-        Temperatura: response.data.main.temp,
-        Umidade: response.data.main.humidity,
-        VelocidadeDoVento: response.data.wind.speed,
-        Clima: clima,
-        Cores: CoresClima[response.data.weather[0].description] || { bg1: 'lightgray', bg2: 'gray' }
-      };
-      res.send(weatherData);
-    } else {
-      res.status(response.status).send({ erro: "Erro ao obter dados metereológicos" });
-    }
-  } catch (error) {
-    res.status(500).send({ erro: "Erro ao obter dados metereológicos", error })
-  }
+            const weatherData = {
+                Temperatura: response.data.main.temp,
+                Umidade: response.data.main.humidity,
+                VelocidadeDoVento: response.data.wind.speed,
+                Clima: clima
+            };
+            res.send(weatherData);
+        } else {
+            res.status(response.status).send({erro: "Erro ao obter dados metereológicos"});
+        }
+    } catch (error) {
+        res.status(500).send({erro: "Erro ao obter dados metereológicos", error})
+    } 
 });
+
--- main.js
+++
@@ -1,9 +0,0 @@
-const div = document.getElementById("movableDiv");
-document.addEventListener("pointermove", moveDiv);
-
-function moveDiv(event) {
-	div.style.left = event.clientX + "px";
-	div.style.top = event.clientY + "px";
-}
-
-// //////////////////////////////////////////////////////////////////
